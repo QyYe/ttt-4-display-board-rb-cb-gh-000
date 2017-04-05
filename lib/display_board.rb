24fda61691daec27caedb57b9517b92c71da9358
Qy Yeoh <qyqianyi95@hotmail.com>
2017-04-05 15:38:40 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,15 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(number)
+  puts " #{number[0]} | #{number[1]} | #{number[2]} "
+  puts "-----------"
+  puts " #{number[3]} | #{number[4]} | #{number[5]} "
+  puts "-----------"
+  puts " #{number[6]} | #{number[7]} | #{number[8]} "
+
+end
+
+number = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+
+display_board(number)
